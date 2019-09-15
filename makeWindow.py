ab314e75a7328dd847cce08a55ed300b6f78f4d4
zawster <mahsan12030@gmail.com>
2019-09-15 18:58:04 +0500

    Make display window to show realtime data

---
+++ makeWindow.py
@@ -0,0 +1,9 @@
+import time
+import psutil
+import matplotlib.pyplot as plt
+%matplotlib notebook
+plt.rcParams['animation.html'] = 'jshtml'
+fig = plt.figure()
+ax = fig.add_subplot(111)
+
+fig.show()
